7cc7b4870e2fe773aa87eca3380a030299d876f8
Grant Knief <gknief@mac.com>
2018-09-27 21:26:20 -0400

    added dope home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -9,22 +9,22 @@ export default class Home extends Component {
         return (
 
                 <div className="home-container">
-
-                    <div className="logo">
-                        <Link to='/'>
-                            <img className="logo" src="../images/fan_star_logo1.png" />
-                            <h1 className="fanstar"><span className="f-in-fanstar">F</span>an <span className="f-in-fanstar">S</span>tar</h1></Link>
-                    </div>
-                    <nav className="home-nav">
+                    {/* <nav className="home-nav">
                         <Link to="/signup">Sign Up</Link>
                         &nbsp;
                         <Link to="/login">Log In</Link>
                         
-                    </nav>
-                    <div className="home-photo">
-                    
-                        <h1>Who's the Number 1 Fan?</h1>
-                        <img className='baseball' src='../images/homepage.jpg' />
+                    </nav> */}
+                    <div className="logo">
+                        {/* <Link to='/'> */}
+
+                            <img className="logo" src="../images/fan_star_logo1.png" />
+                            <h1 className="fanstar"><span className="f-in-fanstar">F</span>an <span className="f-in-fanstar">S</span>tar</h1>
+                            {/* </Link> */}
+                    </div>
+                    <h1>The Most Valuable Fans</h1>
+                    <div className="home-buttons">
+                        <button className='login-button'><Link to='/login'>Log In</Link></button> <br></br><br></br>
                         <button className='signup-button'><Link to='/signup'>Sign Up</Link></button>
                     </div>
                 </div>

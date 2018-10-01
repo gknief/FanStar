4084776cf481f8d8d09d84b3b76cd67ed6f08a1a
Casey Gillan <gillan.casey@gmail.com>
2018-10-01 15:43:43 -0400

    process.env.NODE_ENV

--- server.js
+++ server.js
@@ -215,9 +215,11 @@ app.get('/api/games', async (request, response) => {
   response.json(games);
 })
 
-
-
-
+if (process.env.NODE_ENV == "production") {
+  app.get("/*", function(request, response) {
+    response.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);

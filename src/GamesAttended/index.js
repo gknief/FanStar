fd01fa47b1e52d5c71eae008943d65fe503d7eb8
Casey Gillan <gillan.casey@gmail.com>
2018-10-01 18:03:42 -0400

    updated fan score

--- src/GamesAttended/index.js
+++ src/GamesAttended/index.js
@@ -18,17 +18,11 @@ class GamesAttended extends Component {
   componentDidMount = async () => {
     await this.fetchUser();
     this.fetchGames();
-    console.log('123')
-
   }
 
   fetchGames = async () => {
-    console.log('hello');
     const response = await fetch(`/api/${this.state.user.userId}/userGames`);
-   
-
     const gameList = await response.json();
-    console.log(gameList);
 
     this.setState({
       gameList: gameList,
@@ -47,20 +41,6 @@ class GamesAttended extends Component {
     });
   }
 
-  // addGames = async id => {
-  //   this.setState({
-  //     addEvent: true
-  //   })
-  //   await fetch(`/api/${id}/userGames`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'jwt-token': localStorage.getItem('user_jwt')
-  //     }
-  //   });
-  // }
-
-
   render() {
     return (
       <div className="GameList">
@@ -74,7 +54,6 @@ class GamesAttended extends Component {
               location={game.location}
               awayTeam={game.awayTeam}
               homeTeam={game.homeTeam}
-              // onClick={() => this.addGames(game.id)}
             />
           );
         })}
@@ -82,86 +61,5 @@ class GamesAttended extends Component {
     );
   }
 }
-    
-
-// GameListItem = (props) => {
-//   const setCurrentGame = () => {
-//     props.setCurrentGame(props.id);
-//   }
-
-// const selectedClass = props.currentGame.id === props.id ? 'game-list-item--selected' : '';
-
-//   return (
-//     <div className={`game-list-item ${selectedClass}`} onClick={setCurrentGame}>
-//       <div className="game-list-item-info-wrapper">
-//         <h2 className="game-list-item-title">{props.location}</h2>
-//         <p>{props.awayTeam}</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// GameList = (props) => {
-//   return (
-//     <div className="game-list">
-//       <h1 className="game-list-section-title">GAMES</h1>
-//       {props.gameList.map(game => {
-//         return (
-//           <GameListItem
-//             key={game.id}
-//             key={game.id}
-//             date={game.date}
-//             time={game.time}
-//             location={game.location}
-//             awayTeam={game.awayTeam}
-//             homeTeam={game.homeTeam}
-//             setCurrentGame={props.setCurrentGame}
-//             currentGame={props.currentGame}
-//           />
-//         );
-//       })}
-//     </div>
-//   )
-// }
-
-// GameDetails = (props) => {
-//   return (
-//     <div className="game-details">
-//       <h1 className="game-details-section-title">{props.game.location}</h1>
-//       <h2>{props.game.awayTeam}</h2>
-//     </div>
-//   );
-// }
-
-
-
-
-// render() {
-//   if (this.state.addEvent) {
-//     const { from } = this.props.location.state || { from: { pathname: "/profile" } };
-//     return (
-//       <Redirect to={from} />
-//     )
-//   }
-//   return (
-//     <div className="GameList">
-//       <h1 className="game-list-section">GAMES</h1>
-//       {this.state.gameList.map(game => {
-//         return (
-//           <Game
-//             key={game.id}
-//             date={game.date}
-//             time={game.time}
-//             location={game.location}
-//             awayTeam={game.awayTeam}
-//             homeTeam={game.homeTeam}
-//             onClick={() => this.addGames(game.id)}
-//           />
-//         );
-//       })}
-//     </div>
-//   );
-// }
-// }
 
 export default GamesAttended;
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -12,12 +12,14 @@ class Profile extends Component {
         this.state = {
             user: '',
             games: 0,
-            rank: 0
+            rank: 0,
+            gameList: []
         }
     }
 
-    componentDidMount = () => {
-        this.fetchUser();
+    componentDidMount = async () => {
+        await this.fetchUser();
+        this.fetchGames();
     }
 
     fetchUser = async () => {
@@ -30,8 +32,16 @@ class Profile extends Component {
         this.setState({
             user: user
         });
-        console.log(this.state.user);
     }
+
+    fetchGames = async () => {
+        const response = await fetch(`/api/${this.state.user.userId}/userGames`);
+        const gameList = await response.json();
+    
+        this.setState({
+          gameList: gameList,
+        })
+      }
 
   onInputChange = e => {
     this.setState({
@@ -49,11 +59,11 @@ class Profile extends Component {
         <Link to="/"><button className="logout" onClick={this.logout}>Log Out</button></Link>
         <div>Name: {this.state.user.firstName} {this.state.user.lastName}</div>
         <div>Email: {this.state.user.email}</div>
-        <div>Fan rank: {this.state.rank}</div>
+        <div>Fan Score: {Math.floor(this.state.gameList.length / 1271 * 5000)}</div>
         <div>Favorite team: {this.state.user.favoriteTeam}</div>
         <div><Link to="./gamelist"><button className="add-game-button">Add new game:</button></Link> </div>
-        <div>Number of games attended: {this.state.games}</div>
-        <div>Games I've attended:{}</div>
+        <div>Number of games attended: {this.state.gameList.length}</div>
+        <div>Games I've attended:</div>
         <GamesAttended/>
       </div>
 
